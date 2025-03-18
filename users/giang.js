0cab90ba16743bac536fe8a3dff91006577b1307
unknown <dangtinn306@gmail.com>
2025-03-18 20:37:42 +0700

    Thêm chức năng đăng nhập

---
+++ users/giang.js
@@ -0,0 +1,18 @@
+import {
+    mongo_get,
+    mongo_find_query,
+    mongo_detect_single,
+    mongo_insert_query,
+    mongo_delete_query,
+    mongo_update_single
+} from '../mongo_1/config/main_process.js';
+
+
+export default async function giang() {
+    const query = { "users.pass.id": 3545343 };
+    const set = {
+        $set: { "users.pass.$.value": "1234567" }
+    };
+    const info = await mongo_update_single(query, set);
+    return info;
+}
