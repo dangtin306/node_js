accd6e7a979840fd928c3f65e9d73228d58df407
giangevelyn0118 <giangevelyn0118@gmail.com>
2025-03-21 20:52:31 +0700

    glkdfklglkdfklfdáddas

--- users/giang.js
+++ users/giang.js
@@ -15,15 +15,18 @@ export default async function giang() {
     if (url_full.includes('/giang_1')) {
         const query = { "app_structure.ads_open.device_id": "safssdfdfs" };
         const set = {
-          $set: { "app_structure.ads_open.$.open_views": 5 }
+            $set: { "app_structure.ads_open.$.open_views": 5 }
         };
         const info = await mongo_update_single(query, set);
-        
-        
+
+
         return info;
 
     } else if (url_full.includes('/giang_2')) {
         return 'giang_2';
+    } else if (url_full.includes('/giang_3')) {
+        const info = await mongo_get("app_structure.app_fonted.sidebar_menu.tecom");
+        return info;
     } else {
         return 'ko gi ca';
     }
