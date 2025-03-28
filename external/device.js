5259be3df7452598fd782d9a2be803e53f22ed8b
hustmedia <dangtinn306@gmail.com>
2025-03-28 18:00:08 +0700

    dffdsfdssdfdsfdfs

--- external/device.js
+++ external/device.js
@@ -23,8 +23,8 @@ export default async function device() {
             // console.log(url_param);
             let detect_device = await mongo_detect_single({
                 "external_connect.micro_chip.device_id": device_id,
-                "external_connect.micro_chip.device_type":device_type
-              });
+                "external_connect.micro_chip.device_type": device_type
+            });
             detect_device = detect_device.mongo_results;
             if (detect_device == "detect_yes") {
                 return " có device_id";
@@ -48,6 +48,66 @@ export default async function device() {
         else {
             return "ko có device_id";
 
+        }
+    } else if (url_full.includes('/device_id')) {
+        // console.log(url_param);
+        const device_id = url_param.device_id;
+        const device_type = url_param.device_type;
+
+        if (device_id) {
+            // console.log(url_param);
+            let detect_device = await mongo_detect_single({
+                "external_connect.micro_chip.device_id": device_id,
+                "external_connect.micro_chip.device_type": device_type
+            });
+            detect_device = detect_device.mongo_results;
+            if (detect_device == "detect_yes") {
+                return " có device_id";
+            } else {
+                let query = { "external_connect.micro_chip.device_type": device_type };
+                let maxId = await mongo_json_count(query);
+                maxId = maxId.mongo_results;
+                const newDevice = {
+                    "id": maxId + 1,
+                    "device_id": device_id, // Thay đổi giá trị theo ý bạn
+                    "device_type": device_type,
+                    "device_title": device_id,
+                    "created_date": new Date(),
+                    "ver": 1
+                };
+                query = { "external_connect.micro_chip": newDevice };
+                const info = await mongo_insert_query(query);
+                return "ko có device_id đã thêm vô cơ sở dữ liệu" + info;
+            }
+        }
+        else {
+            return "ko có device_id";
+
+        }
+    } else if (url_full.includes('/devices_users')) {
+        let mongo_status = false;
+        const id_users = data_post_api.id_users;
+        if (id_users) {
+            let id_control = await mongo_find_query({ "media.audio.users.id_users": id_users }, "id_control");
+            // console.log(id_control);
+            mongo_status = id_control.mongo_status;
+            id_control = id_control.mongo_results;
+            if (mongo_status == "success") {
+                const query = { "media.audio.files": { $exists: true } };
+                const field = {
+                    path: "media.audio.files",
+                    filter_field: "id_control",
+                    filter_values: id_control
+                };
+                const result = await mongo_get_multi(query, field);
+                return result;
+                // return info;
+            } else {
+                return "ko có file";
+            }
+        }
+        else {
+            return "ko có id_users";
         }
     }
     else {
