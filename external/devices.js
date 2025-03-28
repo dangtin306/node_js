ef3c7e404faec961b65df2f0855888c01266d40c
hustmedia <dangtinn306@gmail.com>
2025-03-28 18:45:18 +0700

    device_id_insertdfbdfb

--- external/devices.js
+++ external/devices.js
@@ -14,49 +14,14 @@ export default async function devices() {
     const data_post_api = asyncLocalStorage.getStore().get('data_post_api');
     const url_full = asyncLocalStorage.getStore().get('url_full');
     const url_param = asyncLocalStorage.getStore().get('url_param');
-    if (url_full.includes('/device_id')) {
-        // console.log(url_param);
-        const device_id = url_param.device_id;
-        const device_type = url_param.device_type;
-
-        if (device_id) {
-            // console.log(url_param);
-            let detect_device = await mongo_detect_single({
-                "external_connect.devices.device_id": device_id,
-                "external_connect.devices.device_type": device_type
-            });
-            detect_device = detect_device.mongo_results;
-            if (detect_device == "detect_yes") {
-                return " có device_id";
-            } else {
-                let query = { "external_connect.devices.device_type": device_type };
-                let maxId = await mongo_json_count(query);
-                maxId = maxId.mongo_results;
-                const newDevice = {
-                    "id": maxId + 1,
-                    "device_id": device_id, // Thay đổi giá trị theo ý bạn
-                    "device_type": device_type,
-                    "device_title": device_id,
-                    "created_date": new Date(),
-                    "ver": 1
-                };
-                query = { "external_connect.devices": newDevice };
-                const info = await mongo_insert_query(query);
-                return "ko có device_id đã thêm vô cơ sở dữ liệu" + info;
-            }
-        }
-        else {
-            return "ko có device_id";
-
-        }
-    } else if (url_full.includes('/device_id_insert')) {
+    if (url_full.includes('/device_id_insert')) {
         // console.log(url_param);
         const device_id = url_param.device_id;
         const device_type = url_param.device_type;
         const device_model = url_param.device_model;
 
         if (device_id) {
-            // console.log(url_param);
+            // console.log("dsfdfs" + device_model);
             let detect_device = await mongo_detect_single({
                 "external_connect.devices.device_id": device_id,
                 "external_connect.devices.device_model": device_model
