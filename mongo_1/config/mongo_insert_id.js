a7c0d1f15cbf39065c5be2618c6c21bc923dc0ca
hustmedia <dangtinn306@gmail.com>
2025-06-09 22:55:39 +0700

    saddassdaasd

--- mongo_1/config/mongo_insert_id.js
+++ mongo_1/config/mongo_insert_id.js
@@ -1,13 +1,38 @@
-import { mongo_insert_query } from './insert_process.js';
+import {
+  mongo_get,
+  mongo_get_multi,
+  mongo_find_query,
+  mongo_detect_single,
+  mongo_json_count,
+  mongo_insert_query,
+  mongo_delete_query,
+  mongo_update_single,
+  mongo_update_multi
+} from '../mongo_1/config/main_process.js';
 
+function hasIdField(obj) {
+  return Object.prototype.hasOwnProperty.call(obj, 'id');
+}
 
 export async function mongo_insert_id(query, set) {
-  // Tạo object với key động
-  const insertData = {
-    [query]: set
-  };
+  if (hasIdField(set)) {
+    return "lỗi có trường id";
+  } else {
+    // Tạo object với key động
+    let maxId = await mongo_get(query);
+    maxId = maxId.mongo_results;
+    maxId = maxId.reduce((max, data) => data.id > max ? data.id : max, 0);
+    // Clone `set` và gán thêm id
+    const payload = {
+      id: maxId,
+      ...set
+    };
+    const insertData = {
+      [query]: payload
+    };
 
-  // Gọi hàm insert và trả về toàn bộ kết quả
-  const result = await mongo_insert_query(insertData);
-  return result;
+    // Gọi hàm insert và trả về toàn bộ kết quả
+    const result = await mongo_insert_query(insertData);
+    return result;
+  }
 }
