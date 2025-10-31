32fd6ac447079a71801cde49dcc8cac13418a38e
hustmedia <dangtinn306@gmail.com>
2025-10-31 16:50:21 +0700

    adsdasdasadsdas

--- logic/checkscam/check_data.js
+++ logic/checkscam/check_data.js
@@ -10,7 +10,7 @@ import {
 } from '../../mongo_1/config/main_process.js';
 import { asyncLocalStorage } from '../../requestContext.js';
 
-export default async function posts_data() {
+export default async function check_data() {
     const data_post_api = asyncLocalStorage.getStore().get('data_post_api');
     const url_full = asyncLocalStorage.getStore().get('url_full');
     const url_param = asyncLocalStorage.getStore().get('url_param');
@@ -18,15 +18,16 @@ export default async function posts_data() {
     const apikey = url_param.apikey;
     const category_pro = url_param.category_pro;
     const main_domain = url_param.main_domain;
-    const national_market = url_param.national_market;
+    const id_data = data_post_api.id_data;
 
-    if (data_post_api.value === 'test') {
-        const info = await mongo_insert_query({
-            "services.checkscam.datascam.facebook": {"ok": 'test'}
-        }); return info;
-    } 
- 
-    return (data_post_api);
-
+    if (data_post_api.mode === 'detect') {
+        
+        const query = {
+            "services.checkscam.data_scam.facebook.id_data": id_data
+        }
+        const info = await mongo_detect_single(query); return info;
+    } else {
+        return (data_post_api);
+    }
 }
 // ...existing code...
